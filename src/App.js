1735ea61cd5ddcdddcef08027c26a9af6d0610d5
Carlos <carlos01guerrerope@gmail.com>
2024-01-18 22:10:54 -0500

    cambio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import './App.css';
+import Boton from './components/Boton';
+import Contador from './components/Contador';
+import clickBanner from'./img/click-banner.png';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = ()=>{
+    setNumClics(numClics + 1);
+  }; 
+  const reinicarContador = ()=>{
+    setNumClics(0);
+  };
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='contenedor'>
+        <img
+          className='contenedor-logo'
+          src={clickBanner}
+          alt='logo del contenedor'
+        />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics}/>
+        <Boton
+        texto='Clic'
+        esBotonDeClic={true}
+        manejarClic={manejarClic}
+        />
+        <Boton
+        texto='Reiniciar Clic'
+        esBotonDeClic={false}
+        manejarClic={reinicarContador}
+        />
+      </div>
     </div>
   );
 }
